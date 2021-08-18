01e93f4ce2c7193d35ea022328f270ba937ff656
Talatov Akhmad <akhmad2000@hotmail.com>
2021-08-18 22:05:12 +0500

    first commit

---
+++ List&Row/List_RowApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_RowApp.swift
+//  List&Row
+//
+//  Created by Akhmad Talatov on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct List_RowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
